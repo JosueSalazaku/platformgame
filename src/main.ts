25e4e247ca4a2776309d13fbacc94b3555a638de
Josué Salazaku <josuesalazaku@gmail.com>
2024-10-17 01:06:37 +0200

    create classes for game objects

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,59 @@ ctx.fillStyle = "purple";
 ctx.fillRect(20, 20, 20, 20);
 
 
-console.log(ctx.fillStyle)
+
+class Player {
+   position: number;
+   velocity: number;
+   movement: string;
+  
+  constructor (position: number, velocity: number, movement: string) {
+    this.position = position;
+    this.velocity = velocity;
+    this.movement = movement;
+  }
+}
+
+class Platform {
+  position: number;
+  height: number;
+  width: number;
+  material: string;
+
+  constructor(position: number, height: number, width: number, material: string) {
+    this.position = position;
+    this.height = height;
+    this.width = width;
+    this.material = material
+  }
+}
+
+class Obstacles {
+  position: number;
+  height: number;
+  width: number;
+  material: string;
+
+  constructor(position: number, height: number, width: number, material: string) {
+    this.position = position;
+    this.height = height;
+    this.width = width;
+    this.material = material
+  }
+}
+
+class Currency {
+  position: number;
+  value: number;
+  color: string;
+  material: string;
+
+  constructor (position: number, value: number, color: string, material: string) {
+    this.position = position;
+    this.value = value;
+    this.color = color;
+    this.material = material
+  }
+}
+
+
