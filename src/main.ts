ec6c5f6a5420e4fb581220ee6db52a0a327c6e4b
Josué Salazaku <josuesalazaku@gmail.com>
2024-10-17 00:40:01 +0200

    Update canvas position, height and width

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 ctx.fillStyle = "purple";
-ctx.fillRect(10, 10, 10, 10);
+ctx.fillRect(20, 20, 20, 20);
 
 
 console.log(ctx.fillStyle)
--- types.ts
+++
@@ -1,5 +0,0 @@
-
-
-export interface CanvasFillStrokeStyles {
-    fillStyle: string | CanvasGradient | CanvasPattern;
-}
